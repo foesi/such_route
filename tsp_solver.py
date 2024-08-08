866098eef99d2a24c064193d1787a7cd669344f0
Florian Österreich <florian.oesterreich@ossmail.de>
2024-08-08 22:40:07 +0200

    An attempt to refactor the solver to cope with multiple matrices
    Reads all files from results and chooses the best file as result.
    The solver does not plot the solution every time.
    The checkpoint data frame is reduced to only the checkpoints which are in the distance matrix.

--- tsp_solver.py
+++ tsp_solver.py
@@ -1,5 +1,7 @@
+import copy
 import logging
 import math
+import os
 
 from collections import defaultdict
 
@@ -19,8 +21,9 @@ class TspSolver:
         self.latitudes = data['Latitude']
         self.longitudes = data['Longitude']
         self.cantons = data['Canton']
+        self.rearranged_tour = None
 
-        self.nodes = [0] + list(range(1, len(self.latitudes)+1))
+        self.nodes = list(range(0, len(importedDistance)+1))
         self.distances = self.augment_distance(importedDistance)
         self.model = gp.Model()
 
@@ -110,23 +113,23 @@ class TspSolver:
                     <= len(tour) - 1
                 )
 
-    def draw_tsp_solution(self, tour, latitudes, longitudes, cantons):
+    def draw_tsp_solution(self):
         # Create the plot
         plt.figure(figsize=(10, 8))
-        plt.scatter(latitudes, longitudes, c='blue', marker='o')
+        plt.scatter(self.latitudes, self.longitudes, c='blue', marker='o')
 
         # Draw edges:
-        for i in range(len(tour)-1):
-            cur = tour[i]
-            nex = tour[i+1]
+        for i in range(len(self.rearranged_tour)-1):
+            cur = self.rearranged_tour[i]
+            nex = self.rearranged_tour[i+1]
             # k- bedeutet schwarz, durchgezogene Linie mit Stärke (lw) gleich 1
-            plt.plot([latitudes[cur], latitudes[nex]], [
-                     longitudes[cur], longitudes[nex]], 'k-', lw=1)
+            plt.plot([self.latitudes[cur], self.latitudes[nex]], [
+                self.longitudes[cur], self.longitudes[nex]], 'k-', lw=1)
 
         # Annotate each point with its Canton name
-        for i in range(len(cantons)):
-            plt.text(latitudes[i] + 0.01, longitudes[i] +
-                     0.01, cantons[i], fontsize=9, ha='left')
+        for i in range(len(self.cantons)):
+            plt.text(self.latitudes[i] + 0.01, self.longitudes[i] +
+                     0.01, self.cantons[i], fontsize=9, ha='left')
 
         # Set the title and labels
         plt.title('Canton Locations')
@@ -203,8 +206,7 @@ class TspSolver:
             print("")
             assert abs(m.ObjVal - cost) < 1e-5
 
-            self.draw_tsp_solution(
-                rearranged_tour, self.latitudes, self.longitudes, self.cantons)
+            self.rearranged_tour = rearranged_tour
 
             # In case, we only want to return the tour we should return rearranged_tour
             return tour_with_costs, m.ObjVal
@@ -213,21 +215,56 @@ class TspSolver:
 if __name__ == "__main__":
     data = pd.read_csv('checkpoints.csv', sep=';', encoding='utf-8')
 
+    lowest_cost = None
+    shortest_route = None
+    shortest_filename = None
+    shortest_matrix = None
+    copied_data = None
+
     # Load JSON data from a file
-    with open('alle_checkpoints_valhalla_no_feries.json', 'r', encoding='utf-8') as file:
-        importedDistance = json.load(file)
+    for _, _, files in os.walk('results'):
+        for filename in files:
+            with open('results/' + filename, 'r') as file:
+                if not filename.endswith('json'):
+                    continue
+                # Dictionary of Euclidean distance between each pair of points
+                importedDistance = json.load(file)
 
-    # Dictionary of Euclidean distance between each pair of points
+                copied_data = copy.copy(data)
+                for i, line in data.iterrows():
+                    if str((line['Longitude'], line['Latitude'])) not in importedDistance:
+                        copied_data.drop(index=i, inplace=True)
 
-    solver = TspSolver(data, importedDistance)
-    tour, cost = solver.solve()
+                solver = TspSolver(copied_data, importedDistance)
+                tour, cost = solver.solve()
+
+                if lowest_cost:
+                    if cost < lowest_cost:
+                        lowest_cost = cost
+                        shortest_route = tour
+                        shortest_filename = filename
+                        reduced_data = copied_data
+                else:
+                    lowest_cost = cost
+                    shortest_route = tour
+                    shortest_filename = filename
+                    reduced_data = copied_data
 
     # the following code needs to b refactored to be more readable
     # case 1: integrate the following code into the TspSolver class with a new method export_solution or similar
     # case 2: create a new class to handle the export of the solution because we only neeed the best solution from our scrambler
-    data['Order'] = sorted(
-        range(len(tour)), key=lambda x: list(tour.keys())[x])
-    data['Time'] = data['Order'].map(tour)
 
-    data.to_csv('checkpoints_ordered.csv', sep=';',
+    reduced_data['Order'] = sorted(
+        range(len(shortest_route)), key=lambda x: list(shortest_route.keys())[x])
+    reduced_data['Time'] = reduced_data['Order'].map(shortest_route)
+
+    reduced_data.to_csv('checkpoints_ordered.csv', sep=';',
                 encoding='utf-8', index=False)
+
+    # with open('results/' + shortest_filename, 'r') as file:
+    #     importedDistance = json.load(file)
+    #
+    #     solver = TspSolver(data, importedDistance)
+    #     solver.solve()
+    #
+    #     solver.draw_tsp_solution()
