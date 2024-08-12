82f1f09160be263b926c05cd1063ff66404c01a4
Florian Österreich <florian.oesterreich@ossmail.de>
2024-08-12 14:21:45 +0200

    Fix bug: nearest_station did not load cost from cache

--- data/station.py
+++ data/station.py
@@ -11,7 +11,7 @@ RADIUS = 20
 
 
 class NearestStation:
-    def __init__(self, cache: Cache, near_point: Optional[Tuple[float, float]] = None,
+    def __init__(self, cache: Cache, near_point: Tuple[float, float] = None,
                  position: Optional[Tuple[float, float]] = None, routing_algorithm = None):
         self._cache = cache
         self._position = position
@@ -19,31 +19,33 @@ class NearestStation:
         self._cost = None
         if not position:
             if cache_hit := cache.get_generic(f'station:{near_point[0]},{near_point[1]}'):
-                self._position = cache_hit[0]
-                return
-            overpass = Overpass(endpoint='https://overpass.kumi.systems/api/')
-            result = overpass.query(
-                f'(node["railway"="station"](around:{RADIUS * 1000},{near_point[0]},{near_point[1]}););out body geom;',
-                out='json')
-            routing = routing_algorithm if routing_algorithm else Valhalla(cache)
-            if len(result.elements()) == 0:
-                raise ValueError(
-                    f'There is no station in a {RADIUS}km radius, you have to provide the nearest station manually.')
-            for elem in result.elements():
-                coords = elem.geometry().coordinates
-                try:
-                    cost = routing.direct_connection(self._near_point[1], self._near_point[0], coords[0], coords[1])
-                except KeyError:
-                    continue
-                if not self._cost:
-                    self._cost = cost
-                    self._position = (coords[0], coords[1])
-                if cost < self._cost:
-                    self._cost = cost
-                    self._position = (coords[0], coords[1])
-            cache.set_generic(f'station:{near_point[0]},{near_point[1]}', self._position)
-            cache.set_generic(f'station_cost:{near_point[0]},{near_point[1]}', self._cost)
-            cache.save()
+                self._position = cache_hit
+            else:
+                overpass = Overpass(endpoint='https://overpass.kumi.systems/api/')
+                result = overpass.query(
+                    f'(node["railway"="station"](around:{RADIUS * 1000},{near_point[0]},{near_point[1]}););'
+                    f'out body geom;',
+                    out='json')
+                routing = routing_algorithm if routing_algorithm else Valhalla(cache)
+                if len(result.elements()) == 0:
+                    raise ValueError(
+                        f'There is no station in a {RADIUS}km radius,'
+                        f' you have to provide the nearest station manually.')
+                for elem in result.elements():
+                    coords = elem.geometry().coordinates
+                    try:
+                        cost = routing.direct_connection(self._near_point[1], self._near_point[0], coords[0], coords[1])
+                    except KeyError:
+                        continue
+                    if not self._cost:
+                        self._cost = cost
+                        self._position = (coords[0], coords[1])
+                    if cost < self._cost:
+                        self._cost = cost
+                        self._position = (coords[0], coords[1])
+                cache.set_generic(f'station:{near_point[0]},{near_point[1]}', self._position)
+                cache.set_generic(f'station_cost:{near_point[0]},{near_point[1]}', self._cost)
+                cache.save()
         if not self._cost:
             if cache_hit := cache.get_generic(f'station_cost:{near_point[0]},{near_point[1]}'):
                 self._cost = cache_hit
