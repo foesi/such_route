0659c60fb61634170c1e46a3b6714caecdbfb95d
Florian Österreich <florian.oesterreich@ossmail.de>
2024-08-14 10:16:17 +0200

    Refactor routing service to use polygon avoidance in the future but not right now

--- routing_service.py
+++ routing_service.py
@@ -23,7 +23,7 @@ class RoutingService:
     def direct_connection(self, source_lon, source_lat, target_lon, target_lat):
         raise NotImplementedError()
 
-    def _cache_or_connection(self, source_lon, source_lat, target_lon, target_lat):
+    def cache_or_connection(self, source_lon, source_lat, target_lon, target_lat):
         """
         This function returns the shortest distance for the given coordinates.
         It tries to get the distance from the cache, but calculates it otherwise.
@@ -31,26 +31,30 @@ class RoutingService:
         """
         # iterate over the shortest routes for this connection, ignoring the avoided cantons
         # if the route does not enter any cantons to avoid, this will be the shortest connection
-        for (time, distance, route) in sorted(self.cache.get_all((source_lon, source_lat), (target_lon, target_lat)),
-                                              key=lambda x: x[0]):
-            # intersect the route with every nogo canton, if there is no intersection, cache the result
-            hit_nogos = map(lambda x: x.intersect(route), self.nogos)
-            if not any(hit_nogos):
-                if self.nogos:
-                    self.cache.set((time, distance, route), (source_lon, source_lat), (target_lon, target_lat),
-                                   self.nogos)
-                    logger.info(
-                        f'reuse shortest route (start: {(source_lat, source_lon)}, dest: {(target_lat, target_lon)})')
-                return time, distance, route
+    # for (time, distance, route) in sorted(self.cache.get_all((source_lon, source_lat), (target_lon, target_lat)),
+    #                                       key=lambda x: x[0]):
+    #     # intersect the route with every nogo canton, if there is no intersection, cache the result
+    #     hit_nogos = map(lambda x: x.intersect(route), self.nogos)
+    #     if not any(hit_nogos):
+    #         if self.nogos:
+    #             self.cache.set((time, distance, route), (source_lon, source_lat), (target_lon, target_lat),
+    #                            self.nogos)
+    #             logger.info(
+    #                 f'reuse shortest route (start: {(source_lat, source_lon)}, dest: {(target_lat, target_lon)})')
+    #         return time, distance, route
+
         # if there was no previous cache hit, calculate the shortest route
-        (time, distance, route) = self.direct_connection(source_lon, source_lat, target_lon, target_lat)
-        self.cache.set((time, distance, route), (source_lon, source_lat), (target_lon, target_lat), self.nogos)
-        if self.nogos:
-            # save cache for the calculation of routes with cantons to avoid
-            self.cache.save()
-            logger.info(
-                f'calculate route while avoiding cantons '
-                f'(start: {(source_lat, source_lon)}, dest: {(target_lat, target_lon)})')
+        if cache_hit := self.cache.get((source_lon, source_lat), (target_lon, target_lat)):
+            (time, distance, route) = cache_hit
+        else:
+            (time, distance, route) = self.direct_connection(source_lon, source_lat, target_lon, target_lat)
+            self.cache.set((time, distance, route), (source_lon, source_lat), (target_lon, target_lat), self.nogos)
+        # if self.nogos:
+        #     # save cache for the calculation of routes with cantons to avoid
+        #     self.cache.save()
+        #     logger.info(
+        #         f'calculate route while avoiding cantons '
+        #         f'(start: {(source_lat, source_lon)}, dest: {(target_lat, target_lon)})')
         return time, distance, route
 
     def _calc_matrix_from_coordinates(self, coordinates):
@@ -60,7 +64,7 @@ class RoutingService:
                 result[source] = {}
             for target in coordinates:
                 if source != target and source != DEST_COORDS:
-                    result[source][target] = self._cache_or_connection(source[0], source[1], target[0], target[1])[0]
+                    result[source][target] = self.cache_or_connection(source[0], source[1], target[0], target[1])[0]
 
         # make the time to reach any destination from the final destination Bundesplatz in bern very large, so it will
         # be the final destination for sure
